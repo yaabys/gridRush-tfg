bd13489d5b4209c83b5d53839c8c7a31fda25f9a
Alvaro <alvaro@MBP-de-Admin.localdomain>
2025-04-23 13:13:28 +0200

    laanga

--- app/src/pages/seasons/Seasons.jsx
+++ app/src/pages/seasons/Seasons.jsx
@@ -8,7 +8,7 @@ const Seasons = () => {
   const [ranking, setRanking] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+//laaaanaghaaaaa
   useEffect(() => {
     const fetchData = async () => {
       try {
